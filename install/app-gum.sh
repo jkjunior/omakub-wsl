43708875cccc9afa6aa5a16fdc1e00b03614fed6
Jack Setaro <jacksetaro@gmail.com>
2025-08-13 12:26:18 -0600

    try remove version variable

--- install/app-gum.sh
+++ install/app-gum.sh
@@ -1,7 +1,6 @@
 # Gum is used for the Omakub commands for tailoring Omakub after the initial install
 cd /tmp
-GUM_VERSION="0.16.2" # Use known good version
-wget -O gum.deb "https://github.com/charmbracelet/gum/releases/download/v${GUM_VERSION}/gum_${GUM_VERSION}_amd64.deb"
+wget -O gum.deb "https://github.com/charmbracelet/gum/releases/download/v0.16.2/gum_0.16.2_amd64.deb"
 sudo apt install -y ./gum.deb
 rm gum.deb
 cd -
